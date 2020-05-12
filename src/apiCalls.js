5d9fc8b4e89a5cc33b5c2d0d68ff7b2734a93f51
Taras Tarlov <ttarlov@gmail.com>
2020-05-12 16:37:03 -0600

    Switch from localhost to heroku

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,19 +1,18 @@
 export const getAreas = () => {
-    return fetch('http://localhost:3001/api/v1/areas')
+    return fetch('https://vrad-api.herokuapp.com/api/v1/areas')
     .then(res => res.json())
     .catch(err => console.log(err))
 }
 
-export const getAreaDetails = (data) => {
-const areaDetails = data.areas.map(area => {
-        return fetch(`http://localhost:3001${area.details}`)
-        .then(res => res.json())
-        .then(area => {
-            return {
-            name: area.name,
-            ...area
-            }
-        })
-})
-}
-
+// export const getAreaDetails = (data) => {
+// const areaDetails = data.areas.map(area => {
+//         return fetch(`https://vrad-api.herokuapp.com/${area.details}`)
+//         .then(res => res.json())
+//         .then(area => {
+//             return {
+//             name: area.name,
+//             ...area
+//             }
+//         })
+// })
+// }
