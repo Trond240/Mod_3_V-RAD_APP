33975ffd5f1b91ffdd67530c83c9f761e7db558d
Trond Makonese <trondation@gmail.com>
2020-05-12 14:38:47 -0600

    Areas now fetch in component did mount.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { getAreas, getAreaDetails } from '/Users/trondmakonese/mod_3/V-RAD/vrad-project/src/apiCalls.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      areas: []
+    }
+  }
+
+  componentDidMount(){
+    getAreas()
+    .then(data => {
+      console.log(data)
+      const areaDetails = data.areas.map(area => {
+        // getAreaDetails()
+        return fetch(`http://localhost:3001${area.details}`)
+        .then(res => res.json())
+        .then(area => {
+            return {
+              name: area.name,
+              ...area
+            }
+        })
+      })
+      return Promise.all(areaDetails)
+    })
+    .then(areas => this.setState({ areas }))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    console.log(this.state)
+    return(
+      <main>
+        {/* <Switch> */}
+          <h1>Made it</h1>
+        {/* </Switch> */}
+      </main>
+    )
+  }
 }
 
 export default App;
