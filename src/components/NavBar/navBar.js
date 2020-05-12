985ac53a1e13ce2252046ef439b7df9f83ea5ece
Taras Tarlov <ttarlov@gmail.com>
2020-05-12 16:38:23 -0600

    Add NavBar Component

---
+++ src/components/NavBar/navBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => {
+
+return (
+  <nav className='nav-bar'>
+  <h1>Hello World</h1>
+</nav>
+)
+
+
+
+
+
+}
+export default NavBar
