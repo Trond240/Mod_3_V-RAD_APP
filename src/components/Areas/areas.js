3847e567d83e0d8da831d3192ba15400849907e7
Trond Makonese <trondation@gmail.com>
2020-05-13 15:19:03 -0600

    Add areas component.

--- src/components/Areas/areas.js
+++ src/components/Areas/areas.js
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 import '/Users/trondmakonese/mod_3/V-RAD/vrad-project/src/components/Areas/areas.css';
 
 export const Areas = (props) => {
-    console.log(props)
     return (
         <section className='area-card'>
             <h3>{props.name}</h3>
             <h3>Location: {props.locationName}</h3>
             <h3>{props.about}</h3>
-            <Link to={`areas/${props.id}/listings`}>
+            <Link to={`/areas/${props.id}/listings`}>
                 <button id={props.id}>View Listings</button>
             </Link>
         </section>
