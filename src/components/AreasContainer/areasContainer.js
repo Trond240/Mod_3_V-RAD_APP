bfcde9c3ee71e0f85e385b718440f65a06c7ba56
Trond Makonese <trondation@gmail.com>
2020-05-13 11:15:17 -0600

    Aeas conatainer no holds desirec information.

--- src/components/AreasContainer/areasContainer.js
+++ src/components/AreasContainer/areasContainer.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Areas } from '/Users/trondmakonese/mod_3/V-RAD/vrad-project/src/components/Areas/areas.js';
 
 export const AreasContainer = (props) => {
-    console.log(props)
+    const allAreaInfo = props.areaInfo.map(area => {
+        return (
+            <Areas 
+                key={area.id}
+                id={area.id}
+                name={area.name}
+                locationName={area.location}  
+                about={area.about}
+            />
+        )
+    })
 
     return (
     <section>
-        <h1>Made it</h1>
+        {allAreaInfo}
     </section>
     )
 }
