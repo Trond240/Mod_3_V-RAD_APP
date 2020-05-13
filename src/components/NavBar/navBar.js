263d913fb71b62824a5deacafa0102d52849f862
Taras Tarlov <ttarlov@gmail.com>
2020-05-13 15:29:13 -0600

    Add buttons to navigation bar

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -5,7 +5,16 @@ const NavBar = () => {
 
 return (
   <nav className='nav-bar'>
-  <h1>Hello World</h1>
+    
+    <h1>Welcome User Name to your Business Adventure</h1>  
+    <h2>User Name</h2>
+    <h3>User Email Address</h3>
+    <section className='nav-btn-container'>
+      <button>Favorites</button>
+      <button>Areas</button>
+      <button>Listings</button>
+      <button>Log Out</button>
+    </section>
 </nav>
 )
 
