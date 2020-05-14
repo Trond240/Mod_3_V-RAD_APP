6f38ce9487bed9e11a7892ef82c50f00026c08a8
Trond Makonese <trondation@gmail.com>
2020-05-14 15:20:12 -0600

    Listing details files have been created, correct details are passed down to props.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 import { getAreas, getAreaDetails } from '../src/apiCalls.js';
 import LoginPage from './components/LoginPage/LoginPage.js';
 import { AreasContainer } from './components/AreasContainer/areasContainer';
-import {ListingContainer} from './components/ListingContainer/listingContainer.js'
+import {ListingContainer} from './components/ListingContainer/listingContainer.js';
+import { ListingDetails } from './components/ListingDetails/listingDetails.js'
 
 class App extends Component {
   constructor(){
@@ -39,12 +40,18 @@ class App extends Component {
     return(
       <main>
         <Switch>
+          <Route path='/areas/:id/listings/:listingID'render={ ({ match }) => <ListingDetails
+          match={ match }
+          listings={this.state.listingList.filter(listing => {
+            return listing.listing_id === parseInt(match.params.listingID)
+          })}
+          />}/>
           <Route path='/areas/:id/listings' render={ ({ match }) => <ListingContainer 
           listingByArea={this.state.areas.filter(areaListings => 
           areaListings.id === parseInt(match.params.id))}
           match={ match }
           listings={this.state.listingList.filter(listing => listing.area_id === parseInt(match.params.id))}
-          />} />
+          />}/>
           <Route path='/areas' render={ () => <AreasContainer areaInfo={this.state.areas}/>} />
           <Route path='/' exact render={ () => <LoginPage userInfo={this.setUserInfo} />} />
         </Switch>
---
+++ src/components/ListingDetails/listingDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const ListingDetails = (props) => {
+    // console.log(props)
+const allDetails = props.listings.map(listing => {
+        console.log(listing)
+    })
+    return(
+        <section>Made it</section>
+    )
+}
