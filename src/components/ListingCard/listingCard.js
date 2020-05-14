fa3c40ca3c80e4ad0b5e4b64646c5b5c0ca6c4f6
Trond Makonese <trondation@gmail.com>
2020-05-14 12:06:24 -0600

    Add listing cards to the dom, need styling. File path needs update on listing  cards.

--- src/components/ListingCard/listingCard.js
+++ src/components/ListingCard/listingCard.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export const ListingCard = (props) => {
-    console.log(props)
     return (
-        <h1>Made it</h1>
+        <section className='listing-card'>
+            <h2>{props.name}</h2>
+            <Link to={`/areas/${props.areaId}/listings/${props.id}`}>
+                <button id={props.id}>View Listing</button>
+            </Link>
+        </section>
     )
 }
--- src/components/ListingContainer/listingContainer.js
+++ src/components/ListingContainer/listingContainer.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
-// import { ListingCard } from './components/ListingCard/listingCard.js'
+import React from 'react';
+import { ListingCard } from '/Users/trondmakonese/mod_3/V-RAD/vrad-project/src/components/ListingCard/listingCard.js'
 
 export const ListingContainer = (props) => {
     console.log(props)
 
+    const allListings = props.listings.map(listing => {
+        console.log(listing)
+        return(
+            <ListingCard 
+            key={listing.listing_id}
+            id={listing.listing_id}
+            areaId={listing.area_id}
+            name={listing.name}     
+            />
+        )
+    })
+
     return (
     <section>
-        <h1>Made-it</h1>
+        {allListings}
     </section>
     )
 }
