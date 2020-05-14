c1ea31930399e95c54c076b12bcb40379bf5a1ba
Trond Makonese <trondation@gmail.com>
2020-05-14 15:59:27 -0600

    Special features niow show.

--- src/components/Detail-Information/detailInformation.js
+++ src/components/Detail-Information/detailInformation.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export const Details = (props) => {
     console.log(props)
+    const allFeatures = props.features.map(feature => {
+        return (
+        <li>{feature}</li>
+        )
+    })
     return(
         <section>
             <h1>{props.area}</h1>
@@ -10,6 +15,8 @@ export const Details = (props) => {
             <p>Number of Beds: {props.beds}</p>
             <p>Number of bath: {props.baths}</p>
             <p>Cost per night: {props.baths}</p>
+            <h2>Speacial Features:</h2>
+            <ul>{allFeatures}</ul>
             <button id={props.id}>Favorite</button>
         </section>
     )
