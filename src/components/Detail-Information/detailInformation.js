7126e98775ec5568504c3e2352f61a59d0d0440a
Taras Tarlov <ttarlov@gmail.com>
2020-05-15 20:24:23 -0600

    Fix incorrect prop

--- src/components/Detail-Information/detailInformation.js
+++ src/components/Detail-Information/detailInformation.js
@@ -8,13 +8,14 @@ export const Details = (props) => {
         )
     })
     return(
+        
         <section>
             <h1>{props.area}</h1>
             <p>Street: {props.street}</p>
             <p>Zip: {props.zip}</p>
             <p>Number of Beds: {props.beds}</p>
             <p>Number of bath: {props.baths}</p>
-            <p>Cost per night: {props.baths}</p>
+            <p>Cost per night: ${props.cost}</p>
             <h2>Speacial Features:</h2>
             <ul>{allFeatures}</ul>
             <button id={props.id}>Favorite</button>
