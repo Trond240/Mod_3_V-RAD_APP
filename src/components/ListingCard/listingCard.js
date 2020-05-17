fcdda841f6d54b113d2b8c04085e60debb10cc94
Taras Tarlov <ttarlov@gmail.com>
2020-05-16 20:04:28 -0600

    Import css for component

--- src/components/ListingCard/listingCard.js
+++ src/components/ListingCard/listingCard.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './ListingCrad.css'
 
 export const ListingCard = (props) => {
     return (
         <section className='listing-card'>
             <h2>{props.name}</h2>
             <Link to={`/areas/${props.areaId}/listings/${props.id}`}>
-                <button id={props.id}>View Details</button>
+                <button id={props.id} >View Details</button>
             </Link>
         </section>
     )
