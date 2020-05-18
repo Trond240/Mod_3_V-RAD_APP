1ed8f46991f44ca2a8565bae05392c14f03a57d6
Taras Tarlov <ttarlov@gmail.com>
2020-05-18 08:30:07 -0600

    Add style class to view details btn

--- src/components/ListingCard/listingCard.js
+++ src/components/ListingCard/listingCard.js
@@ -8,7 +8,7 @@ export const ListingCard = (props) => {
         <section className='listing-card'>
             <h2>{props.name}</h2>
             <Link to={`/areas/${props.areaId}/listings/${props.id}`}>
-                <button id={props.id} >View Details</button>
+                <button className='view-details-btn' id={props.id} >View Details</button>
             </Link>
         </section>
     )
