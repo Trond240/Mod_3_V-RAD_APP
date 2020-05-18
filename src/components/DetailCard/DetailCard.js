4816466537a44cf3cff9adf1fbd4908a6949ae21
Taras Tarlov <ttarlov@gmail.com>
2020-05-17 18:10:11 -0600

    Add conditional logic for favorite/unfavorite button

---
+++ src/components/DetailCard/DetailCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './detailCard.css'
+
+export const Details = (props) => {
+    console.log(props)
+    let favorites = props.favorites; 
+    
+    let favoriteOrUnfavoriteBtn;
+
+
+    if(favorites.includes(props.id)) {
+        favoriteOrUnfavoriteBtn = <button id={props.id} onClick={() => props.removeFromFavorites(props.id)}>Unfavorite</button> 
+    } else {
+        favoriteOrUnfavoriteBtn = <button id={props.id} onClick={() => props.addToFavorites(props.id)}>Favorite</button>
+    }
+
+    const allFeatures = props.features.map(function (feature, index) {
+        return (
+        <li key ={index}>{feature}</li>
+        )
+    })
+    return(
+        
+        <section className='detail-card'>
+            <h1>{props.area}</h1>
+            <p>Street: {props.street}</p>
+            <p>Zip: {props.zip}</p>
+            <p>Number of Beds: {props.beds}</p>
+            <p>Number of bath: {props.baths}</p>
+            <p>Cost per night: ${props.cost}</p>
+            <h2>Speacial Features:</h2>
+            <ul>{allFeatures}</ul>
+            {favoriteOrUnfavoriteBtn}
+        </section>
+    )
+}
+
+Details.propTypes = {
+    area: PropTypes.string,
+}
