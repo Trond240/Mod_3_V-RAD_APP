5f8c87758455dabc3fa2389cb2cde705813eb63e
Taras Tarlov <ttarlov@gmail.com>
2020-05-17 18:11:33 -0600

    pass props for favariting function

--- src/components/ListingDetails/listingDetails.js
+++ src/components/ListingDetails/listingDetails.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Details } from '../Detail-Information/DetailInformation';
+import { Details } from '../DetailCard/DetailCard';
 import PropTypes from 'prop-types';
 import './listingDetails.css'
 
 export const ListingDetails = (props) => {
+    let allDetails;
 
-    
-const allDetails = props.listings.map(listing => {
+    if (props.favorites.length ===0) {
+        allDetails = <p>No Favorites Found</p>
+    } else {
+            allDetails = props.listings.map(listing => {
         return (
-            <Details 
-                key={listing.listing_id}
-                area={listing.area}
-                id={listing.listing_id}
-                street={listing.address.street}
-                zip={listing.address.zip}
-                baths={listing.details.baths}
-                beds={listing.details.beds}
-                cost={listing.details.cost_per_night}
-                features={listing.details.features}
-                addToFavorites = {props.addToFavorites}
-            />
+        <Details 
+            key={listing.listing_id}
+            area={listing.area}
+            id={listing.listing_id}
+            street={listing.address.street}
+            zip={listing.address.zip}
+            baths={listing.details.baths}
+            beds={listing.details.beds}
+            cost={listing.details.cost_per_night}
+            features={listing.details.features}
+            addToFavorites = {props.addToFavorites}
+            favorites = {props.favorites}
+            removeFromFavorites = {props.removeFromFavorites}
+        />
         )
-    })
+        })
+    }
     return(
         <section className='listing-details-container'>
             {allDetails}
