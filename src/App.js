d0820d1632c9d5bd2c7203180f9c12f96d9138ea
Taras Tarlov <ttarlov@gmail.com>
2020-05-17 18:09:16 -0600

    add methods for favoriting functionality

--- src/App.js
+++ src/App.js
@@ -26,9 +26,22 @@ class App extends Component {
   addToFavorites = (id) => {
       this.setState({favorites:[...this.state.favorites,id]})
       
-      console.log("favorite btn pressed")
   }
 
+  removeFromFavorites = (id) => {
+    let favorites = this.state.favorites.filter(favoriteId => favoriteId !== id )
+    this.setState({favorites})
+  }
+
+  findFavorites = () => {
+
+    return this.state.listingList.filter(listing => {
+
+        return this.state.favorites.find(favoriteId => favoriteId === listing.listing_id)
+
+      })
+    
+  }
 
   componentDidMount(){
       getAreas()
@@ -47,28 +60,47 @@ class App extends Component {
   render() {
 
     let navBar;
-    Object.keys(this.state.user).length === 0 ? navBar = "" : navBar = <NavBar user ={this.state.user} favorites={this.state.favorites} setUserInfo={this.setUserInfo} />
+    Object.keys(this.state.user).length === 0 ? navBar = "" : navBar = <NavBar 
+    user ={this.state.user} 
+    favorites={this.state.favorites} 
+    setUserInfo={this.setUserInfo} 
+    />
 
 
     return(
       <main className='main-section'>
              {navBar}  
         <Switch>
-          <Route path='/areas/:id/listings/:listingID'render={ ({ match }) => <ListingDetails addToFavorites = {this.addToFavorites}
+         
+          <Route path='/areas/:id/listings/:listingID'render={ ({ match }) => <ListingDetails 
+          favorites = {this.state.favorites}  
+          addToFavorites = {this.addToFavorites}
+          removeFromFavorites = {this.removeFromFavorites}
           match={ match }
           listings={this.state.listingList.filter(listing => {
             return listing.listing_id === parseInt(match.params.listingID)
           })}
           />}/>
+         
           <Route path='/areas/:id/listings' render={ ({ match }) => <ListingContainer 
           listingByArea={this.state.areas.filter(areaListings => 
           areaListings.id === parseInt(match.params.id))}
           match={ match }
           listings={this.state.listingList.filter(listing => listing.area_id === parseInt(match.params.id))}
           />}/>
+         
           <Route path='/areas' render={ () => <AreasContainer areaInfo={this.state.areas}/>} />
+         
           <Route path='/' exact render={ () => <LoginPage setUserInfo={this.setUserInfo} />} />
-          {/* <Route path = '/favorites' render ={() => } /> */}
+         
+          <Route path = '/favorites' render={ ({ match }) => <ListingDetails 
+          addToFavorites = {this.addToFavorites} 
+          removeFromFavorites = {this.removeFromFavorites}
+          favorites = {this.state.favorites}
+          match={ match }
+          listings= {this.findFavorites()}
+          />} />
+        
         </Switch>
       </main>
     )
