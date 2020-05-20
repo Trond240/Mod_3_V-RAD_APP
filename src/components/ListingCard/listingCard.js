8403de58c1ba9e87932dfbc960b4b5d13571eb64
Taras Tarlov <ttarlov@gmail.com>
2020-05-20 17:04:33 -0600

    Add Classes for styling

--- src/components/ListingCard/listingCard.js
+++ src/components/ListingCard/listingCard.js
@@ -8,7 +8,8 @@ export const ListingCard = (props) => {
 
     return (
         <section className='listing-card'>
-            <h2>{props.name}</h2>
+            <h2 className="listing-card-title">{props.name}</h2>
+            <img className="listing-img" src={`/images/${props.id}_a.jpg`} alt={`Images of ${props.steet}`} />
             <Link to={`/areas/${props.areaId}/listings/${props.id}`}>
                 <button className='view-details-btn' id={props.id} >View Details</button>
             </Link>
