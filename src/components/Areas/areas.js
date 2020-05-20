8a6e2d6463661cc8de551e672066cd3633c723b3
Taras Tarlov <ttarlov@gmail.com>
2020-05-20 17:03:28 -0600

    Add classes for styling

--- src/components/Areas/areas.js
+++ src/components/Areas/areas.js
@@ -10,9 +10,9 @@ export const Areas = (props) => {
   
     return (
         <section className='area-card'>
-            <h3>{props.name}</h3>
+            <h3 className="areas-title">{props.name}</h3>
             <h3>Location: {props.locationName}</h3>
-            <h3>{props.about}</h3>
+            <p className="about-paragraph">{props.about}</p>
             <Link to={`/areas/${props.id}/listings`}>
                 <button className='view-listing-btn' id={props.id}>View Listings</button>
             </Link>
